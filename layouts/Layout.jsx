c0e8aacd59c038deb9ce3ba841f4242835f06182
gerebabh <geralucio@gmail.com>
2026-06-21 03:54:20 -0300

    feat: cria componente navbar. Fecha #7

--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -1,4 +1,5 @@
 import { Link, Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
 
 function Layout() {
   return (
@@ -10,28 +11,7 @@ function Layout() {
           </Link>
         </div>
 
-        <nav
-          aria-label="Navegacao principal"
-          className="bg-[#1E293B]"
-        >
-          <ul className="mx-auto flex w-full max-w-7xl flex-wrap gap-4 px-4 py-3 text-sm sm:gap-6 sm:px-6 lg:px-8">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/cadastro">Cadastro</Link>
-            </li>
-            <li>
-              <Link to="/veiculos">Veiculos</Link>
-            </li>
-            <li>
-              <Link to="/combustivel">Combustiveis</Link>
-            </li>
-            <li>
-              <Link to="/comparador">Comparador</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
       </header>
 
       <main className="mx-auto w-full max-w-7xl flex-1 px-4 py-6 sm:px-6 sm:py-8 lg:px-8">
